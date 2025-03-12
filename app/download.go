a9bb11278d68b4fe8c0b55ad64294e1d0a76e0b2
Lux Barker <29106316+k-nox@users.noreply.github.com>
2025-03-12 09:44:04 -0700

    feat: add options to change output template and directory

--- app/download.go
+++ app/download.go
@@ -2,9 +2,14 @@ package app
 
 import (
 	"context"
+	"fmt"
+	"path/filepath"
 
 	"github.com/lrstanley/go-ytdlp"
+	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
+
+const defaultOutputTemplate = "%(title)s [%(id)s].%(ext)s"
 
 type Downloader struct {
 	ctx context.Context
@@ -14,11 +19,30 @@ func (d *Downloader) ensureInstalled() {
 	ytdlp.MustInstall(d.ctx, &ytdlp.InstallOptions{})
 }
 
-func (d *Downloader) Download(url string) error {
-	_, err := ytdlp.New().FormatSort("res:1080").Run(d.ctx, url)
+func (d *Downloader) Download(url string, outputDir string, outputTempl string) error {
+	cmd := ytdlp.New().FormatSort("res,vcodec:h264,acodec:m4a")
+
+	if outputTempl == "" {
+		outputTempl = defaultOutputTemplate
+	}
+
+	cmd.Output(filepath.Join(outputDir, outputTempl))
+
+	_, err := cmd.Run(d.ctx, url)
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func (d *Downloader) ChooseDirectory() (string, error) {
+	dir, err := runtime.OpenDirectoryDialog(d.ctx, runtime.OpenDialogOptions{
+		Title:                "Choose directory to save downloads",
+		CanCreateDirectories: true,
+	})
+	if err != nil {
+		return "", fmt.Errorf("error choosing directory: %w", err)
+	}
+	return dir, nil
 }
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,25 +1,48 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
 	import { Input } from '$lib/components/ui/input';
-	import { Download } from '$lib/wailsjs/go/app/Downloader';
+	import { Label } from '$lib/components/ui/label';
+	import { ChooseDirectory, Download } from '$lib/wailsjs/go/app/Downloader';
 	import { LogDebug } from '$lib/wailsjs/runtime/runtime';
-	let url = $state('');
+	const form = $state({
+		url: '',
+		outputTempl: '',
+		outputDir: ''
+	});
 
 	let status = $state('');
+
+	const resetForm = () => {
+		form.url = '';
+		form.outputTempl = '';
+		form.outputDir = '';
+	};
 
 	const download = async () => {
 		status = 'loading';
 		try {
-			await Download(url);
-		} catch (e) {
+			await Download(form.url, form.outputDir, form.outputTempl);
+		} catch (e: unknown) {
 			if (typeof e === 'string') {
 				LogDebug(e);
 			} else if (e instanceof Error) {
 				LogDebug(e.message);
 			}
 		}
+		resetForm();
 		status = '';
-		url = '';
+	};
+
+	const chooseDirectory = async () => {
+		try {
+			form.outputDir = await ChooseDirectory();
+		} catch (e: unknown) {
+			if (typeof e === 'string') {
+				LogDebug(e);
+			} else if (e instanceof Error) {
+				LogDebug(e.message);
+			}
+		}
 	};
 </script>
 
@@ -28,8 +51,37 @@
 		<div class="flex min-h-full flex-col items-center justify-center">loading...</div>
 	{:else}
 		<form class="flex min-h-full flex-col items-center justify-center gap-4">
-			<Input class="w-1/3" type="text" placeholder="video url" bind:value={url} />
-			<Button type="button" onclick={download}>Download</Button>
+			<div class="flex w-full justify-center gap-4">
+				<div class="w-1/3">
+					<Label for="url" class="m-1">URL</Label>
+					<Input id="url" type="url" placeholder="video url" bind:value={form.url} />
+				</div>
+				<div class="w-1/3">
+					<Label for="templ" class="m-1">Output Template</Label>
+					<Input
+						id="templ"
+						type="text"
+						placeholder="%(title)s [%(id)s].%(ext)s"
+						bind:value={form.outputTempl}
+					/>
+				</div>
+			</div>
+			<div class="flex w-full flex-col items-center">
+				<Label for="dir" class="m-1">Output Directory</Label>
+				<div class="flex w-1/2 rounded-md border border-input shadow-sm">
+					<Button variant="secondary" type="button" onclick={chooseDirectory}
+						>Choose Directory</Button
+					>
+					<Input
+						id="dir"
+						class="border-0 focus-visible:ring-0"
+						readonly
+						type="text"
+						bind:value={form.outputDir}
+					/>
+				</div>
+			</div>
+			<Button type="submit" onclick={download}>Download</Button>
 		</form>
 	{/if}
 </div>
