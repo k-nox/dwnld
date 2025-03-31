0fafda8dc63c2f5cb9373995fa7872ad9f367bf4
Lux Barker <29106316+k-nox@users.noreply.github.com>
2025-03-31 15:37:22 -0700

    add button for settings

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,15 +1,23 @@
 <script lang="ts">
 	import ThemeToggle from '$lib/components/theme-toggle.svelte';
 	import { Toaster } from '$lib/components/ui/sonner';
+	import Settings from 'lucide-svelte/icons/settings';
 	import '../app.css';
 	import { ModeWatcher } from 'mode-watcher';
+	import { Button } from '$lib/components/ui/button';
+	import { EventsEmit } from '$lib/wailsjs/runtime/runtime';
 	let { children } = $props();
 </script>
 
 <ModeWatcher />
 <Toaster richColors />
 <div class="flex h-screen flex-col">
-	<header class="flex flex-row items-center justify-end p-6" style="--wails-draggable:drag">
+	<header class="flex flex-row items-center justify-end gap-6 p-6" style="--wails-draggable:drag">
+		<div style="--wails-draggable:nodrag">
+			<Button variant="outline" size="icon" onclick={() => EventsEmit('openSettings')}
+				><Settings /></Button
+			>
+		</div>
 		<div style="--wails-draggable:nodrag">
 			<ThemeToggle />
 		</div>
